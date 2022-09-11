d1ef7398cd78d6229cd6148cc15648a1db7cbc19
league <league@imac-20.attlocal.net>
2022-09-11 11:26:29 -0700

    IDK

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -12,34 +12,38 @@ public class PentagonCrazy {
 	public static void main(String[] args) {
 		
 		//  Create a new Robot
+		Robot rob = new Robot();
 
 		//  Put the robot's pen down
+		rob.penDown();
 
 		//  SPEED. Make the robot go at maximum speed (100)
+		rob.setSpeed(100);
 
 		//  COLOR. Set the pen to a color that you like for the shape
-
+		rob.setPenColor(66, 135, 245);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape will have.
 		//  				(Hint: its called PentagonCrazy)
-
+		int P = 5;
+		
 		//  TURN ANGLE. Make another int variable for the angle the robot must turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has to get the angle.
-		
+		int X = 360/5;
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-
+		int A = 0;
 		//  LOOP. Start a while loop to repeat the DRAW, TURN, and INCREASE COUNT code 200 times. 
-		
+		while (A < 200) {
 				//  DRAW.  Make the robot move the amount in your count variable
-		
+		rob.move(A);
 				//  TURN.  Turn the robot the amount in your angle variable + 1
-
+		rob.turn(X + 1);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		A++;
 		//  End the while loop here
-
+		}
 		//  Now hide the robot so you can see the pattern it has drawn.
-		
+		rob.hide();
 		//  Run the program. 
 		//  Check the pattern against the picture in the recipe. If it matches, you are done!
 	
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,44 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String input = JOptionPane.showInputDialog("What is 1+1");
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if (input.equals("2")) {
+					score ++;
+				}
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+				String input3 = JOptionPane.showInputDialog("What is 346 * 1000");
+				if (input3.equals("346000")) {
+					score ++;
+				}
+				
+				String input4 = JOptionPane.showInputDialog("What is 125 * 8");
+				if (input4.equals("1000")) {
+					score ++;
+				}
+				
+				String input5 = JOptionPane.showInputDialog("What is 2 * 3 * 4 * 5");
+				if (input5.equals("120")) {
+					score ++;
+				}
+				
+				String input7 = JOptionPane.showInputDialog("What is 1020304050607080 + 600521000 * 567430204 * 24973870943763 * 0");
+				if (input7.equals("0")) {
+					score ++;
+				}
 		// After all the questions have been asked, tell the user their final score 
-		
+				JOptionPane.showMessageDialog(null, score + "/ 5");
 	}
 }
